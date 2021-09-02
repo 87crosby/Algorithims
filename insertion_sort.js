2b0a3f1315a26a19902b78258e616d22b44dd92d
87crosby <71791452+87crosby@users.noreply.github.com>
2021-09-02 20:00:25 +0400

    Add files via upload

---
+++ insertion_sort.js
@@ -0,0 +1,13 @@
+const insertionSort = (arr)=> {
+    for(var i = 1; i < arr.length; i++){
+        let j = i
+        while(arr[j]< arr[j-1]){
+            [arr[j], arr[j-1]]=[arr[j-1], arr[j]]
+            j--
+        }
+    }
+    return arr
+    }
+
+
+console.log(insertionSort([4,7,1,2,5,0,9,6])) 
